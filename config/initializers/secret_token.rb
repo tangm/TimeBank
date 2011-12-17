8becc12d0dfbeea6df5fed7fb48ad708da2dbd3b
Martha Winata <marthawinata@gmail.com>
2011-12-17 16:13:46 +1100

    Initial commit
    Signed-off-by: Martha Winata <marthawinata@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeBank::Application.config.secret_token = '710d12de95f8d8781d0f9d176d7d379a8814e998524a4530927e028c18acc1adc1a7c12e1804e19d94cfe45134163187ed46b42538ab74f67713271e12670650'
